4e3a6028c846948d76ff1168c1b6b401f7dec967
SrimanTetali <tetalisriman@gmail.com>
2024-08-28 12:22:17 +0530

    Added speech_to_text.py to handle PyAudio dependency

---
+++ speech/speech_to_text.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+
+def speech_to_text():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = recognizer.listen(source)
+        try:
+            text = recognizer.recognize_google(audio)
+            return text
+        except sr.UnknownValueError:
+            return "Sorry, I did not understand that."
+        except sr.RequestError:
+            return "Sorry, there was a problem with the request."
